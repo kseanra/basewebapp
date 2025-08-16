86f219eeb2ee19a165370acd870ca768376f2fb1
kliang <kenliangnz@mail.com>
2025-08-16 13:28:18 +1200

    add controller tests

--- basicapi-tests/ControllerTests.cs
+++ basicapi-tests/ControllerTests.cs
@@ -1,10 +1,33 @@
-﻿namespace basicapi_tests;
+﻿using Xunit;
+using Moq;
+using basicapi.Controllers;
+using basicapi.Models;
+using MediatR;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using basicapi.Models;
 
-public class UnitTest1
+namespace basicapi_tests
 {
-    [Fact]
-    public void Test1()
+    public class UserControllerTests
     {
+        [Fact]
+        public async Task CreateUser_ReturnsCreatedAtActionResult_WhenValidRequest()
+        {
+            // Arrange
+            var mediatorMock = new Mock<IMediator>();
+            var mapperMock = new Mock<IMapper>();
+            var controller = new UserController(mediatorMock.Object, mapperMock.Object);
+            var userRequest = new basicapi.Models.UserRequest { Name = "Test User", Email = "test@example.com" };
 
+            mediatorMock.Setup(m => m.Send(It.IsAny<object>(), default)).ReturnsAsync(Unit.Value);
+
+            // Act
+            var result = await controller.CreateUser(userRequest);
+
+            // Assert
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+            Assert.Equal("Test User", createdResult.Value);
+        }
     }
 }
